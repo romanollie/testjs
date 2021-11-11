e50a89f85b797e8b459b878eed3a0eefac0f242c
Olzhas Romazanov <96romrom@gmail.com>
2021-11-11 18:22:46 +0600

    added html in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 import '../css/style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello, Kolesa Upgrade!</h1>
-  <div id="error"></div>
+  <h1>Hello, Kolesa Upgrade!</h1><div id="error"></div>
   <div id="loader" style="display: block"></div>
   <button>Получить</button>
 `;
