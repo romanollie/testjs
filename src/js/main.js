284a68123937936bb4bd8b91fc7e12930d5107c3
Olzhas Romazanov <96romrom@gmail.com>
2021-11-11 18:21:40 +0600

    added jest

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ import '../css/style.css';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello, Kolesa Upgrade!</h1>
+  <div id="error"></div>
+  <div id="loader" style="display: block"></div>
+  <button>Получить</button>
 `;
